b4f4858736f54ac514947b071d04f5d474227949
Yash-Raj-5424 <168925493+Yash-Raj-5424@users.noreply.github.com>
2025-02-12 16:07:22 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all the JS code as newer versions 
+
+// alert(3 + 3) // we are using nodejs not browser
+let name = "yash" // string => can be enclosed in a single quote also
+let age = 19
+
+//          Primitive DataTypes
+// number
+// bigint
+// string => enclosed in either '' or ""
+// boolean => true/false
+// null => standalone value
+// undefined => when value is not assigned
+
+// objects
+
+console.log(typeof "yash"); // directly the value can be passed to check 
+console.log(typeof age); // we can also check the type by using the variable name
+
+console.log(typeof null); // the type of null is object
+console.log(typeof undefined); // the type of undefined is undefined only
+
+
+
