dccd3db1248865ad6552eee7cebf5f117550ad03
Yash-Raj-5424 <168925493+Yash-Raj-5424@users.noreply.github.com>
2025-02-12 18:38:47 +0000

    conversions and operations both

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -43,8 +43,8 @@ let nameInNum = Number(name)
 let isLoggedIn = "avc"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn); // the typeof says it is converted to boolean
-console.log(booleanIsLoggedIn); // and it is actually also converted to boolean only
+// console.log(typeof booleanIsLoggedIn); // the typeof says it is converted to boolean
+// console.log(booleanIsLoggedIn); // and it is actually also converted to boolean only
 
 // 0 => false; 1 => true; also for any value other than 0, the boolean conversion is true
 // "" => false; "abc" => true
@@ -52,5 +52,33 @@ console.log(booleanIsLoggedIn); // and it is actually also converted to boolean 
 // string conversions
 let num = 234
 let stringNum = String(num)
-console.log(num);
-console.log(typeof stringNum); // it is actually converted to a string
+// console.log(num);
+// console.log(typeof stringNum); // it is actually converted to a string
+
+// ********************** Operations ***********************
+
+let val = 5
+let negVal = -val
+// console.log(negVal);
+
+// other arithematic operations such as +, -, *, **, /, % can be done as well
+
+// console.log(2 + "4");
+// console.log("2" + 4);
+// console.log("2" + 3 + 1);
+// console.log(2 + 1 + "4");
+// console.log(2 + 1 + "4" + 5 + 1);
+
+// wherever the string starts, the values next to it are added as string and the values before it are added as numbers (general arithematic addition)
+
+// console.log(true); // gives true
+// console.log(+true) gives 1; console.log(+"") gives 0
+// console.log(true+) is not allowed
+
+let runs = 100
+// runs++;
+// console.log(runs);
+++runs;
+console.log(runs);
+
+
