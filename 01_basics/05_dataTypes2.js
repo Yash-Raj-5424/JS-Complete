6a53405c95070a20063a8f82a57d10142fb41f12
Yash-Raj-5424 <168925493+Yash-Raj-5424@users.noreply.github.com>
2025-02-14 17:45:59 +0000

    datatypes and stack & heap concepts

--- 01_basics/05_dataTypes2.js
+++ 01_basics/05_dataTypes2.js
@@ -6,12 +6,12 @@
 const score = 1234
 const age = Symbol('1234')
 const age2 = Symbol('1213') // Symbol returns a data type whose type is symbol
-console.log(age == age2);
-console.log(typeof age); // gives symbol
+// console.log(age == age2);
+// console.log(typeof age); // gives symbol
 
 
 const myId = null
-console.log(typeof myId); // It gives null
+// console.log(typeof myId); // It gives null
 
 
 const bigNum = 41543453562134535214n // it is automatically stored as big integer
@@ -37,4 +37,31 @@ let myFunction = function(){
 
 // all the non primitive have a typeof object but the type of a function is a function object
 
+
+// ******************* Memory Management *************************************************
+
+// The primitive data types are stored in the stack memory and when we want a reference of that variable, a shallow copy of the actual one is returned and any changes made via reference only reflects in the reference variable but not in the actual variable
+
+let myName = "yash"
+let newName = myName
+// console.log(newName);
+
+newName = "raj"
+// console.log(newName);
+// console.log(myName); // the actual variable is not changed, as we get a copy of the actual variable and hence the changes made reflect in the copied variable not the actual one
+
+// The non primtive data types are stored in the heap memory and when we want its reference, the reference to the actual variable itself is returned. Hence, any changes made to the reference variable also reflects in the actual variable
+
+let user1 = { // here the variable user1 has a reference to the object stored in the heap memory
+    userEmail : "user@gmail.com",
+    upiId : "user@ybl"
+}
+
+let user2 = user1 // here the variable user2 also has the reference to the same object in the heap memory and no extra copy is created for referencing.
+
+user2.upiId = "user2@okAxis" 
+// Any change made via a reference variable reflects in the actual object also since it has the reference to the actual object itself (in heap memory)
+
+console.log(user2.upiId);
+console.log(user1.upiId);
 
